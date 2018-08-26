0c694bf3cc0439aef5ac154565c2be63b415a330
zubair <zubairabuhasan@gmail.com>
2018-08-26 12:39:53 +0800

    Log with NLog

--- AspDotNetCoaching/Controllers/HomeController.cs
+++ AspDotNetCoaching/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLog;
 
 namespace AspDotNetCoaching.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
+            Logger.Info("This is a log.");
+
             var aNumber = int.Parse("abc");
 
             return View();
